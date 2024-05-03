144429c06654a9721639d27fc9e00dcc92b49d00
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-05-02 20:03:32 -0400

    reword

--- frontend/src/metabase/admin/performance/components/StrategyForm.tsx
+++ frontend/src/metabase/admin/performance/components/StrategyForm.tsx
@@ -369,7 +369,7 @@ const StrategySelector = ({
           <Text
             lh="1rem"
             color="text-medium"
-          >{t`Select the cache invalidation policy:`}</Text>
+          >{t`Pick the policy for when cached query results should be invalidated:`}</Text>
         }
         name="type"
       >
